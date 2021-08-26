b2b94989a68b1cfcc431d4e8ac80ff1aa6db8e29
Yonerfy <yonerfy15@gmail.com>
2021-08-26 15:16:09 +0200

    Set the files

---
+++ farm.js
@@ -0,0 +1,15 @@
+const corn = {
+  name: "corn",
+  yield: 30,
+  factors: {
+    sun: {
+      low: -50,
+      medium: 0,
+      high: 50,
+    },
+  },
+};
+
+const environmentFactors = {
+  sun: "low",
+};
---
+++ farm.test.js
@@ -0,0 +1,53 @@
+const { getYieldForPlant, getYieldForCrop, getTotalYield } = require("./farm");
+
+describe("getYieldForPlant", () => {
+  const corn = {
+    name: "corn",
+    yield: 30,
+  };
+
+  test("Get yield for plant with no environment factors", () => {
+    expect(getYieldForPlant(corn)).toBe(30);
+  });
+});
+
+describe("getYieldForCrop", () => {
+  test("Get yield for crop, simple", () => {
+    const corn = {
+      name: "corn",
+      yield: 3,
+    };
+    const input = {
+      crop: corn,
+      numCrops: 10,
+    };
+    expect(getYieldForCrop(input)).toBe(30);
+  });
+});
+
+describe("getTotalYield", () => {
+  test("Calculate total yield with multiple crops", () => {
+    const corn = {
+      name: "corn",
+      yield: 3,
+    };
+    const pumpkin = {
+      name: "pumpkin",
+      yield: 4,
+    };
+    const crops = [
+      { crop: corn, numCrops: 5 },
+      { crop: pumpkin, numCrops: 2 },
+    ];
+    expect(getTotalYield({ crops })).toBe(23);
+  });
+
+  test("Calculate total yield with 0 amount", () => {
+    const corn = {
+      name: "corn",
+      yield: 3,
+    };
+    const crops = [{ crop: corn, numCrops: 0 }];
+    expect(getTotalYield({ crops })).toBe(0);
+  });
+});
